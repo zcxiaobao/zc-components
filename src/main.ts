90ecf278d11fd773359ca0d1b7f37ac864b89477
zcxiaobao <444186976@qq.com>
2023-08-07 17:53:36 +0800

    feat:init styles

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "./styles/index.scss";
+
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import * as Icons from "@element-plus/icons-vue";
